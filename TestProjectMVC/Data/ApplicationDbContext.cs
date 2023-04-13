15891bed6f19d532df97a0762c45743dde11afbf
abdullahdevsinc02 <abdullah.tahir@devsinc.com>
2023-04-14 03:52:58 +0500

    db setup and category initialize

---
+++ TestProjectMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TestProjectMVC.Models;
+
+namespace TestProjectMVC.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext( DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Category> categories { get; set; }
+	}
+}
+
