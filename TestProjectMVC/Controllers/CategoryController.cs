ceed909b190909b193fc5399a1c59f0b7cf3c26f
abdullahdevsinc02 <abdullah.tahir@devsinc.com>
2023-05-08 15:27:06 +0500

    PRODUCTS CRUD

---
+++ TestProjectMVC/Controllers/CategoryController.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestProject.DataAccessLayer.Data;
+using TestProject.Models;
+
+namespace TestProjectMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _db.categories;
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Edit( int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _db.categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated Successfully!";
+
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.categories.Add(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category created Successfully!";
+
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _db.categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteData(int? id)
+        {
+            var category = _db.categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _db.categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully!";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- TestProjectMVC/Controllers/HomeController.cs
+++ TestProjectMVC/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using TestProjectMVC.Models;
+using TestProject.Models;
 
 namespace TestProjectMVC.Controllers;
 
